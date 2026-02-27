70371dcb906fd13332b19d6bc69c8361c85897dc
RGO <rglink98@gmail.com>
2026-02-27 12:40:03 +0600

    Add files via upload

---
+++ BottomNav.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const BottomNav = () => {
+  return (
+    <div style={{
+      position: "fixed",
+      bottom: 0,
+      width: "100%",
+      background: "#111",
+      color: "white",
+      textAlign: "center",
+      padding: "10px"
+    }}>
+      Bottom Navigation
+    </div>
+  );
+};
+
+export default BottomNav;
