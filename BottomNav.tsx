a3bd916147b2e44559a4c6c7763281c873de6634
RGO <rglink98@gmail.com>
2026-02-27 12:41:46 +0600

    Update BottomNav.tsx

--- BottomNav.tsx
+++ BottomNav.tsx
@@ -1,19 +1,3 @@
-import React from "react";
-
-const BottomNav = () => {
-  return (
-    <div style={{
-      position: "fixed",
-      bottom: 0,
-      width: "100%",
-      background: "#111",
-      color: "white",
-      textAlign: "center",
-      padding: "10px"
-    }}>
-      Bottom Navigation
-    </div>
-  );
-};
-
-export default BottomNav;
+git add .
+git commit -m "add BottomNav"
+git push
